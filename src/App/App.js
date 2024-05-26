e33cbf27ee22139cfa8dced5e2592725e46c802d
Nithya_Varshini <nithyavarshini23@gmail.com>
2024-05-26 20:32:15 +0530

    files_created

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,41 @@
-import './App.css';
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BrowserRouter as Router,
+  useLocation,
+} from "react-router-dom";
+import withRouter from "../hooks/withRouter";
+import AppRoutes from "./routes";
+import Headermain from "../assets/header";
 import AnimatedCursor  from "../hooks/AnimatedCursor";
+import "./App.css";
 
-function App() {
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
+}
+const ScrollToTop = withRouter(_ScrollToTop);
+
+export default function App() {
   return (
-    <>
-    <h1>Hello, world!</h1>
-    <div className="cursor__dot">
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="cursor__dot">
         <AnimatedCursor
           innerSize={15}
           outerSize={15}
-          color="255, 255, 0"
+          color="255, 255 ,255"
           outerAlpha={0.4}
           innerScale={0.7}
           outerScale={5}
         />
       </div>
-    </>
+      <ScrollToTop>
+        <Headermain />
+        <AppRoutes />
+      </ScrollToTop>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            {/* Your portfolio content goes here */}
+        </div>
+    );
+};
+
+export default Portfolio;
